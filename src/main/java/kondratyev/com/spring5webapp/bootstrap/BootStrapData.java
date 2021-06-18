f32f5c356591a9b0c0f7786506e75f111b4bce93
kondratyev-a <alex.a.kondratyev@gmail.com>
2021-06-18 16:56:58 +0700

    Added Publisher

--- src/main/java/kondratyev/com/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/kondratyev/com/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package kondratyev.com.spring5webapp.bootstrap;
 
 import kondratyev.com.spring5webapp.domain.Author;
 import kondratyev.com.spring5webapp.domain.Book;
+import kondratyev.com.spring5webapp.domain.Publisher;
 import kondratyev.com.spring5webapp.repositories.AuthorRepository;
 import kondratyev.com.spring5webapp.repositories.BookRepository;
+import kondratyev.com.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +14,26 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        System.out.println("Started in Bootstrap");
+
+        Publisher publisher = new Publisher();
+        publisher.setName("SFG Publishing");
+        publisher.setCity("St Petersburg");
+        publisher.setState("FL");
+
+        publisherRepository.save(publisher);
+        System.out.println("Publisher Count: " + publisherRepository.count());
 
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
@@ -37,7 +51,6 @@ public class BootStrapData implements CommandLineRunner {
         authorRepository.save(rod);
         bookRepository.save(noEJB);
 
-        System.out.println("Started in Bootstrap");
         System.out.println("Number of Books " + bookRepository.count());
 
     }
